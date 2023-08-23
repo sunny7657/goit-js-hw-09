92a2af59746f36f8d676668a0da33b9e4c90eae5
sunny7657 <ksvetlana466@gmail.com>
2023-08-23 19:07:05 +0200

    01-task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
-console.log(object);
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener('click', handlerStart);
+stopBtn.addEventListener('click', handlerStop);
+
+let timerId = null;
+stopBtn.disabled = true;
+
+function handlerStart() {
+  document.body.style.background = getRandomHexColor();
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function handlerStop() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
